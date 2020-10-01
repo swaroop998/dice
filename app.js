cf703a77ec0a7b9d1b3bcaa9253689f2ed395648
naba <naranbatt@gmail.com>
2020-10-01 22:54:29 +0800

    js code bichih

--- app.js
+++ app.js
@@ -36,13 +36,71 @@ document.querySelector(".btn-roll").addEventListener("click", function () {
     document.getElementById("current-" + activePlayer).textContent = "0";
     roundScore = 0;
 
-    activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-
-    document.querySelector(".player-0-panel").classList.toggle("active");
-    document.querySelector(".player-1-panel").classList.toggle("active");
-
-    diceDom.style.display = "none";
+    changePlayer();
   }
 });
 
-//
+//холд дарах эвент литэнэр
+document.querySelector(".btn-hold").addEventListener("click", function () {
+  console.log("hold clicked");
+
+  score[activePlayer] += roundScore;
+  document.getElementById("score-" + activePlayer).textContent =
+    score[activePlayer];
+
+  if (score[activePlayer] > 11) {
+    document.getElementById("name-" + activePlayer).textContent = "WINNER!!!!!";
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.add("winner");
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.remove("active");
+
+    document.querySelector(".btn-hold").style.display = "none";
+    document.querySelector(".btn-roll").style.display = "none";
+  } else {
+    roundScore = 0;
+    document.getElementById("current-" + activePlayer).textContent = "0";
+    changePlayer();
+  }
+});
+
+function changePlayer() {
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+
+  diceDom.style.display = "none";
+}
+
+document.querySelector(".btn-new").addEventListener("click", function () {
+  intitGame();
+});
+
+function intitGame() {
+  activePlayer = 0;
+  roundScore = 0;
+  score = [0, 0];
+  document.getElementById("score-0").textContent = "0";
+  document.querySelector("#score-1").textContent = "0";
+  document.querySelector("#current-0").textContent = "0";
+  document.querySelector("#current-1").textContent = "0";
+
+  document.querySelector(".btn-hold").style.display = "block";
+  document.querySelector(".btn-roll").style.display = "block";
+
+  document.querySelector(".player-0-panel").classList.remove("winner");
+  document.querySelector(".player-1-panel").classList.remove("winner");
+
+  document.querySelector(".player-0-panel").classList.remove("active");
+  document.querySelector(".player-1-panel").classList.remove("active");
+
+  document.querySelector(".player-0-panel").classList.add("active");
+
+  document.getElementById("name-0").textContent = "PLAYER 1";
+  document.getElementById("name-1").textContent = "PLAYER 2";
+
+  diceDom.style.display = "none";
+}
